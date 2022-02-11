037896478860ea2575b372ee348ff06c1490d70e
Dewey Sasser <dewey@deweysasser.com>
2022-02-10 22:42:13 -0500

    refactor: change logging init

--- program/program.go
+++ program/program.go
@@ -3,32 +3,64 @@ package program
 import (
 	"fmt"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"os"
 )
 
-// Version is created by the Makefile and passed in as a linker flag
+// Version is created by the Makefile and passed in as a linker flag.  When go 1.18 is released, this will be replaced
+// with the built-in mechanism
+
 var Version = "unknown"
 
 // Options is the structure of program options
 type Options struct {
-	Debug   bool `short:"d" help:"Show debugging information"`
-	Version bool `short:"v" help:"Show program version"`
-	Quiet   bool `short:"q" help:"Be less verbose than usual"`
+	Debug     bool   `short:"d" help:"Show debugging information"`
+	Version   bool   `short:"v" help:"Show program version"`
+	LogFormat string `short:"l" enum:"auto,jsonl,terminal" default:"auto" help:"How to show program output (auto|terminal|jsonl)"`
+	Quiet     bool   `short:"q" help:"Be less verbose than usual"`
 }
 
 // Run runs the program
 func (program *Options) Run() error {
+	program.initLogging()
 
+	return nil
+}
+
+func (program *Options) initLogging() {
 	if program.Version {
 		fmt.Println(Version)
 		os.Exit(0)
 	}
 
-	if program.Debug {
+	switch {
+	case program.Debug:
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	} else {
+	case program.Quiet:
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	default:
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 
-	return nil
+	if program.LogFormat == "terminal" ||
+		(program.LogFormat == "auto" && isTerminal(os.Stdout)) {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+	} else {
+		log.Logger = log.Output(os.Stdout)
+	}
+
+	log.Logger.Debug().
+		Str("version", Version).
+		Str("program", os.Args[0]).
+		Msg("Starting")
+}
+
+// isTerminal returns true if the file given points to a character device (i.e. a terminal)
+func isTerminal(file *os.File) bool {
+	if fileInfo, err := file.Stat(); err != nil {
+		log.Err(err).Msg("Error running stat")
+		return false
+	} else {
+		return (fileInfo.Mode() & os.ModeCharDevice) != 0
+	}
 }
