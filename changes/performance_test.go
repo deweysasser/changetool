1044ed991d37ceb0d84702555227d75eb333f6d9
Dewey Sasser <dewey@deweysasser.com>
2022-02-18 00:59:50 -0500

    test: move cloning external repo into test_framework

--- changes/performance_test.go
+++ changes/performance_test.go
@@ -1,14 +1,9 @@
 package changes
 
 import (
-	"fmt"
-	"github.com/go-git/go-git/v5"
+	"github.com/deweysasser/changetool/test_framework"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-	"os"
-	"path"
 	"testing"
 )
 
@@ -17,42 +12,7 @@ var repoURL = "https://github.com/rails/rails.git"
 var createdTagCount = 5000
 
 func BenchmarkLargeRepos(b *testing.B) {
-
-	var repo *git.Repository
-
-	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
-		_ = os.MkdirAll(path.Dir(repoPath), os.ModePerm|os.ModeDir)
-		log.Debug().Str("url", repoURL).Msg("Cloning Repo")
-		repo, err = git.PlainClone(repoPath, true, &git.CloneOptions{URL: repoURL})
-		if err != nil {
-			b.Fatal(err)
-		}
-
-		// Rails doesn't have as many tags as we want, so let's create a few thousand
-		var commits []plumbing.Hash
-		if iter, err := repo.Log(&git.LogOptions{Order: git.LogOrderCommitterTime}); err != nil {
-			b.Fatal(err)
-		} else {
-			iter.ForEach(func(commit *object.Commit) error {
-				commits = append(commits, commit.Hash)
-				return nil
-			})
-		}
-
-		log.Debug().Int("count", createdTagCount).Msg("Creating extra tags")
-		tagEvery := len(commits) / createdTagCount
-		for i := 0; i < len(commits); i += tagEvery {
-			repo.Storer.SetReference(plumbing.NewHashReference(plumbing.ReferenceName(fmt.Sprintf("refs/tags/scale-test-tag-%d", i)), commits[i]))
-		}
-
-	} else {
-		log.Debug().Msg("Using existing repo")
-
-		repo, err = git.PlainOpen(repoPath)
-		if err != nil {
-			b.Fatal(err)
-		}
-	}
+	repo := test_framework.CloneRepo(b, repoPath, repoURL, createdTagCount)
 
 	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
 
---
+++ test_framework/clone.go
@@ -0,0 +1,65 @@
+package test_framework
+
+import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/rs/zerolog/log"
+	"os"
+	"path"
+	"testing"
+	"time"
+)
+
+func CloneRepo(b *testing.B, repoPath, repoURL string, createdTagCount int) *git.Repository {
+
+	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
+		_ = os.MkdirAll(path.Dir(repoPath), os.ModePerm|os.ModeDir)
+		log.Debug().Str("url", repoURL).Msg("Cloning Repo")
+		now := time.Now()
+
+		repo, err := git.PlainClone(repoPath, true, &git.CloneOptions{URL: repoURL})
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		log.Debug().Dur("clone_time", time.Since(now)).Msg("Repo cloned")
+
+		// Rails doesn't have as many tags as we want, so let's create a few thousand
+		log.Debug().Msg("Creating tags")
+		now = time.Now()
+		var commits []plumbing.Hash
+		if iter, err := repo.Log(&git.LogOptions{Order: git.LogOrderCommitterTime}); err != nil {
+			b.Fatal(err)
+		} else {
+			if err := iter.ForEach(func(commit *object.Commit) error {
+				commits = append(commits, commit.Hash)
+				return nil
+			}); err != nil {
+				b.Fatal(err)
+			}
+		}
+
+		log.Debug().Int("count", createdTagCount).Msg("Creating extra tags")
+		tagEvery := len(commits) / createdTagCount
+		for i := 0; i < len(commits); i += tagEvery {
+			if err := repo.Storer.SetReference(plumbing.NewHashReference(plumbing.ReferenceName(fmt.Sprintf("refs/tags/scale-test-tag-%d", i)), commits[i])); err != nil {
+				b.Fatal(err)
+			}
+		}
+
+		log.Debug().Dur("tag_time", time.Since(now)).Msg("Tags Created")
+
+		return repo
+	} else {
+		log.Debug().Msg("Using existing repo")
+
+		repo, err := git.PlainOpen(repoPath)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		return repo
+	}
+}
